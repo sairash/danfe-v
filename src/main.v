50b464c2685561b02f9f784072c5b67ef4451f1c
Sai On Action <gautamsairash@gmail.com>
2024-10-16 03:17:46 +0545

    updated parser main file

--- src/main.v
+++ src/main.v
@@ -1,7 +1,5 @@
 module main
-
-import lexer
-import token
+import parser
 import os
 import cli
 
@@ -18,15 +16,8 @@ fn main() {
 				name:          'run'
 				required_args: 1
 				execute:       fn (cmd cli.Command) ! {
-					mut lex := lexer.Lex.new(cmd.args[0])!
-					lex.add_new_file_to_lex('./test_inputs/lexer_test/test1.df', cmd.args[0])!
-					for {
-						token_val := lex.next()!
-						if token_val.token_type == token.TokenType(token.EOF{}) {
-							break
-						}
-						println(token_val)
-					}
+					pars := parser.Parse.new(cmd.args[0])!
+					println(pars)
 					return
 				}
 			},
