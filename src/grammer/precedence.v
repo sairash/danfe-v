6cabe9da7403f258cc61001fb7f440e777f71a16
Sai On Action <gautamsairash@gmail.com>
2024-10-17 05:09:28 +0545

    precedence needed to be changed for parsing

--- src/grammer/precedence.v
+++ src/grammer/precedence.v
@@ -1,6 +1,6 @@
 module grammer
 
-enum PrecedenceValue {
+enum PrecedenceValue as int {
 	lowest
 	equals
 	or_op
@@ -14,23 +14,44 @@ enum PrecedenceValue {
 	prefix
 }
 
-pub const precedence = {
-	"*": PrecedenceValue.multiply,
-	"/": PrecedenceValue.multiply,
-	"%": PrecedenceValue.multiply,
-	"+": PrecedenceValue.add,
-	"-": PrecedenceValue.add,
-	"<<": PrecedenceValue.bit_shift, // Bitwise Left Shift
-	">>": PrecedenceValue.bit_shift, // Bitwise Right Shift
-	"&": PrecedenceValue.bit_and, // Bitwise And
-	"^": PrecedenceValue.bit_or, // Bitwise XOR
-	"==": PrecedenceValue.less_greater,
-	"!=": PrecedenceValue.less_greater,
-	"<": PrecedenceValue.less_greater,
-	"<=": PrecedenceValue.less_greater,
-	">": PrecedenceValue.less_greater,
-	">=": PrecedenceValue.less_greater,
-	"&&": PrecedenceValue.and,
-	"||": PrecedenceValue.or_op,
-}
+// pub const precedence = {
+// 	"*": PrecedenceValue.multiply,
+// 	"/": PrecedenceValue.multiply,
+// 	"%": PrecedenceValue.multiply,
+// 	"+": PrecedenceValue.add,
+// 	"-": PrecedenceValue.add,
+// 	"<<": PrecedenceValue.bit_shift, // Bitwise Left Shift
+// 	">>": PrecedenceValue.bit_shift, // Bitwise Right Shift
+// 	"&": PrecedenceValue.bit_and, // Bitwise And
+// 	"^": PrecedenceValue.bit_or, // Bitwise XOR
+// 	"==": PrecedenceValue.less_greater,
+// 	"!=": PrecedenceValue.less_greater,
+// 	"<": PrecedenceValue.less_greater,
+// 	"<=": PrecedenceValue.less_greater,
+// 	">": PrecedenceValue.less_greater,
+// 	">=": PrecedenceValue.less_greater,
+// 	"&&": PrecedenceValue.and,
+// 	"||": PrecedenceValue.or_op,
+// }
 
+
+
+pub const precedence = {
+	"*": 9,
+	"/": 9,
+	"%": 9,
+	"+": 8,
+	"-": 8,
+	"<<": 7, // Bitwise Left Shift
+	">>": 7, // Bitwise Right Shift
+	"&": 6, // Bitwise And
+	"^": 5, // Bitwise XOR
+	"==": 4,
+	"!=": 4,
+	"<": 4,
+	"<=": 4,
+	">": 4,
+	">=": 4,
+	"&&": 3,
+	"||": 2,
+}
