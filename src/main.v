383c1bbfbd187ba59a6b10d75d68fbb63b08cf58
Sai On Action <gautamsairash@gmail.com>
2024-10-31 10:31:45 +0545

    updated main path to intrepret multiple files using recursion

--- src/main.v
+++ src/main.v
@@ -5,6 +5,7 @@ import os
 import cli
 import cli_df
 import repl
+import ast
 
 fn main() {
 	mut app := cli.Command{
@@ -21,19 +22,28 @@ fn main() {
 				name:          'run'
 				required_args: 1
 				execute:       fn (cmd cli.Command) ! {
-					mut pars := parser.Parse.new(cmd.args[0])!
-					pars.walk()!
-					pars_current := pars.file_process[pars.cur_file] or { return error('') }
-					pars_current_body := pars_current.ast.body
-
-					for i := 0; i < pars_current_body.len; i += 1 {
-						pars_current_body[i].eval('')!
-					}
-					return
+					return interpreter(os.abs_path(parser.format_path(cmd.args[0])), 'main')
 				}
 			},
 		]
 	}
 	app.setup()
 	app.parse(os.args)
+}
+
+fn interpreter(path string, module_name string) ! {
+	mut pars := parser.Parse.new(path, module_name)!
+	pars.walk_main()!
+
+	for i := 0; i < pars.ast.body.len; i += 1 {
+		cur := pars.ast.body[i]
+		match cur {
+			ast.ImportStatement {
+				interpreter(cur.path, '${cur.from_module_}.${cur.module_}')!
+			}
+			else {
+				cur.eval('')!
+			}
+		}
+	}
 }
