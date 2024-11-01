20c02c213034dd9ca202fbd807efe992c9d290bb
Sai On Action <gautamsairash@gmail.com>
2024-11-02 04:37:51 +0545

    reserved function update

--- src/ast/call_expression.v
+++ src/ast/call_expression.v
@@ -6,12 +6,8 @@ fn print_reserved_function(process_id string, args []Node, new_line bool) ! {
 	for arg in args {
 		eval_output := arg.eval(process_id)!
 
-		output_str := match eval_output {
-			string { eval_output }
-			int, f64 { eval_output.str() }
-		}
 
-		print('${output_str}')
+		print('${eval_output.get_as_string()}')
 	}
 	if new_line {
 		print('\n')
@@ -21,11 +17,6 @@ fn print_reserved_function(process_id string, args []Node, new_line bool) ! {
 fn input_reserved_function(process_id string, arg Node) !EvalOutput {
 	eval_output := arg.eval(process_id)!
 
-	output_str := match eval_output {
-		string { eval_output }
-		int, f64 { eval_output.str() }
-	}
-
-	print(output_str)
+	print(eval_output.get_as_string())
 	return os.get_line()
 }
